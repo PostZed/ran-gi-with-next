2b02ab01148e132fbf121c0a1e22c3caa16f0f34
PostZed <130866971+PostZed@users.noreply.github.com>
2026-05-14 09:09:05 +0300

    Add link to my Github.

--- src/app/mini-layout.tsx
+++ src/app/mini-layout.tsx
@@ -4,6 +4,9 @@ export default function Layout({ children }: Readonly<{ children: React.ReactNod
         <div className="bg-white w-full md:w-1/2 lg:w-1/3 flex flex-col mx-auto p-0.2 md:max-xl:border-black border-2
         h-screen relative">
             {children}
+            <p className="mt-4 mx-auto">Made by
+                <a className="hover:underline text-purple-500"
+                    target="_blank" href="https://github.com/PostZed"> Ndunge Muga</a></p>
         </div>
     );
 }
